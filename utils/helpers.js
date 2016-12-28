7410c8732b3049e9b3d7341b5fb42652fb89c7c7
Mohamed Ez zaouia <ezzaouia.mohamed@gmail.com>
2016-12-28 20:30:43 +0100

    fixing module.exports minor error

--- utils/helpers.js
+++ utils/helpers.js
@@ -10,15 +10,10 @@ function root(...args) {
   return path.join(path.resolve(__dirname, '..'), ...args);
 }
 
-function exportModule(envConfigFilename) {
-  module.exports = require(envConfigFilename)
-}
-
 function getWebpackEnvConf(__ENV) {
   let WPConf = root('config', 'webpack');
   return `${WPConf}/webpack-${__ENV}.config.js`;
 }
 
 exports.root = root;
-exports.exportModule = exportModule;
 exports.getWebpackEnvConf = getWebpackEnvConf;
--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,17 @@ const helpers = require('./utils/helpers');
 // load webpack config by env: dev || prod || test
 switch (process.env.NODE_ENV) {
   case 'prod':
-    helpers.exportModule(helpers.getWebpackEnvConf('prod'));
+    exportModule(helpers.getWebpackEnvConf('prod'));
     break;
   case 'test':
-    helpers.exportModule(helpers.getWebpackEnvConf('test'));
+    exportModule(helpers.getWebpackEnvConf('test'));
     break;
   case 'dev':
   default:
-    helpers.exportModule(helpers.getWebpackEnvConf('dev'));
+    exportModule(helpers.getWebpackEnvConf('dev'));
     break;
+}
+
+function exportModule(envConfigFilename) {
+  module.exports = require(envConfigFilename)
 }
